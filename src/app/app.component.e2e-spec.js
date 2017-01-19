42cc24028da374369b25a632518d79e093b0180a
blakeparkinson <wizardplow@gmail.com>
2017-01-18 17:51:18 -0700

    first commit for robyns site

---
+++ src/app/app.component.e2e-spec.js
@@ -0,0 +1,27 @@
+describe('App', function () {
+
+  beforeEach(function () {
+    browser.get('/');
+  });
+
+  it('should have a title', function () {
+    expect(browser.getTitle()).toEqual("Angular 2 App | ng2-webpack");
+  });
+
+  it('should have <header>', function () {
+    expect(element(by.css('my-app header')).isPresent()).toEqual(true);
+  });
+
+  it('should have <main>', function () {
+    expect(element(by.css('my-app main')).isPresent()).toEqual(true);
+  });
+
+  it('should have a main title', function () {
+    expect(element(by.css('main h1')).getText()).toEqual('Hello from Robyns Website!');
+  });
+
+  it('should have <footer>', function () {
+    expect(element(by.css('my-app footer')).getText()).toEqual("Developed by Blake Parkinson");
+  });
+
+});
